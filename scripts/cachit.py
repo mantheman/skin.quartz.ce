dc3101010ece8df846e228eeb8fdbc271d996712
nonJon <nonJon@users.noreply.github.com>
2018-05-06 14:35:10 -0500

    TCM special protocol

--- scripts/cachit.py
+++ scripts/cachit.py
@@ -4,52 +4,21 @@ import time
 import os
 import subprocess
 import sys
- 
-#addon       = xbmcaddon.Addon()
-#addonname   = addon.getAddonInfo('name')
 
 use_shell = False
-appid = "Kodi"
-
-atv2_path = "/User/Library/Preferences/%s/addons/skin.quartz.ce-master/scripts/" % appid
-macosx_path = "~/Library/Application Support/%s/addons/skin.quartz.ce-master/scripts/" % appid
-linux1_path = "/var/lib/%s/.%s/addons/skin.quartz.ce-master/scripts/" % (appid.lower(), appid.lower())
-linux2_path = "~/.%s/addons/skin.quartz.ce-master/scripts/" % appid.lower()
-android1_path = "Android/data/org.%s.%s/files/.%s/addons/skin.quartz.ce-master/scripts/" % (appid.lower(), appid.lower(), appid.lower())
-android2_path = "/sdcard/%s/skin.quartz.ce-master/scripts/" % android1_path
-firetv_path = "/storage/emulated/0/%s/skin.quartz.ce-master/scripts/" % android1_path
-
 if sys.platform == "win32":
-	win32_path = "%s\\%s\\addons\\skin.quartz.ce-master\\scripts\\" % (os.environ["appdata"], appid)
-	if os.path.exists(win32_path):
-		path =  win32_path
-		use_shell = True
-elif sys.platform == "darwin" and os.path.exists(atv2_path):
-		path = atv2_path
-elif sys.platform == "darwin" and os.path.exists(os.path.expanduser(macosx_path)):
-		path = macosx_path
-else: #Linux/Android
-	if os.path.exists(os.path.expanduser(linux1_path)):
-		path = os.path.expanduser(linux1_path)
-	elif os.path.exists(os.path.expanduser(linux2_path)):
-		path = os.path.expanduser(linux2_path)
-	elif os.path.exists(firetv_path):
-		path = firetv_path
-	elif os.path.exists(android2_path):
-		path = android2_path
-	elif os.path.exists(android1_path):
-		path = android1_path
+	use_shell = True
 
-path = path + "texturecache.py"
+loc = xbmc.translatePath("special://skin//scripts//texturecache.py")
 
-answer = xbmcgui.Dialog().yesno('Texture Cache Maintenance utility', 'This may take awhile to run.', '', 'Sure you want to continue?')               
-if not answer: sys.exit(0)
+rc = xbmcgui.Dialog().yesno('Texture Cache Maintenance utility', 'This may take awhile to run.', '', 'Sure you want to continue?')
+
+if not rc: sys.exit(0)
  
 dialog = xbmcgui.DialogProgress()
 dialog.create('Texture Cache Maintenance utility', ("Initiating"))
 
-p = subprocess.Popen(['python', path, 'c'], 
-	bufsize=1, shell=use_shell, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+p = subprocess.Popen(['python', loc, 'c'], bufsize=1, shell=use_shell, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 while True:
 	line = p.stdout.readline()
@@ -63,7 +32,6 @@ while True:
 streamdata = p.communicate()[0]
 rc = p.returncode		
 p.stdout.close()
-
 time.sleep(4)
 
 if (not rc) and dialog.iscanceled(): 
